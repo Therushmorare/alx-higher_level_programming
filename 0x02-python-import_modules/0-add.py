fd918c0dc2dd442e3b849e3b0a97cc1f009fa2b1
Tshegofatso Morare <tshego.tt.morare@gmail.com>
2023-08-11 18:49:10 +0000

    err fixing

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-from add_0 import add
 
-a = 1
-b = 2
+if __name__ == "__main__":
+	from add_0 import add
 
-final_answer = add(a, b)
-print("{} + {} = {}".format(a, b, final_answer))
+	"""print sum"""
+
+	a = 1
+	b = 2
+
+	final_answer = add(a, b)
+	print("{} + {} = {}".format(a, b, final_answer))
