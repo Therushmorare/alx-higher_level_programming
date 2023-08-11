3091709fdbd74c9274289be60c56f5855af4b911
Tshegofatso Morare <tshego.tt.morare@gmail.com>
2023-08-11 19:00:36 +0000

    err fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 
-import sys
+if __name__ == "__main__":
+	""" print arguments """
+	import sys
 
-arguments = len(sys.argv) - 1
-args = sys.argv[1:]
+	arguments = len(sys.argv) - 1
+	args = sys.argv[1:]
 
-if arguments == 0:
-    print("0 arguments.")
+	if arguments == 0:
+		print("0 arguments.")
 
-if arguments == 1:
-    print("1 argument:")
-    print("1: {}".format(args[0]))
+	if arguments == 1:
+		print("1 argument:")
+		print("1: {}".format(args[0]))
 
-if arguments > 1:
-    print("{} arguments:".format(arguments))
-    for i, arg in enumerate(args, start=1):
-        print("{}: {}".format(i, arg))
+	if arguments > 1:
+		 print("{} arguments:".format(arguments))
+	for i, arg in enumerate(args, start=1):
+		print("{}: {}".format(i, arg))
