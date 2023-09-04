93b0c2b382b995f499eb834e248708e197b769a7
Tshegofatso Morare <tshego.tt.morare@gmail.com>
2023-09-04 22:30:54 +0000

    task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
+""" class def"""
 class Rectangle:
+    """ initialize class
+         Args:
+             width (int): width of rectangle
+             height (int): height of rectangle
+    """
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
-        """ initialize the class
-	    Args:
-		width(int): width of rectangle
-		height(int): height of rectangle
-	"""
         self.__width = width
         self.__height = height
         Rectangle.number_of_instances += 1
