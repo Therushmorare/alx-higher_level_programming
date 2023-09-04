a69a0f643d0f1f49f660a686d069a2dbada31677
Tshegofatso Morare <tshego.tt.morare@gmail.com>
2023-09-04 21:47:28 +0000

    2 fix

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -19,6 +19,10 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
+        """
+          Args:
+              value (int): the value received
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -32,6 +36,10 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """
+          Args:
+              value (int): the value received
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
