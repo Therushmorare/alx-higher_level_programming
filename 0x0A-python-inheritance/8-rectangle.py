8e506d9398286e05cc174adff0b43c6a16b9bb5a
Tshegofatso Morare <tshego.tt.morare@gmail.com>
2023-09-11 18:39:51 +0000

    inheritance

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,37 +1,14 @@
 #!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-""" base geo class """
-class BaseGeometry:
-    """ base geo class """
-    def area(self):
-    """ function area """
-        raise Exception("area() is not implemented")
-    def integer_validator(self, name, value):
-        """ integer validator
-            Args:
-                name (string): string to be entered
-                value (int): integer to be validated
-        """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-""" class rectrange that inherits base """
+"""
+module with class Rectangle
+"""
 class Rectangle(BaseGeometry):
-    """ class rectrange that inherits base geo """
+    """Rectangle class that inherits from BaseGeometry"""
+
     def __init__(self, width, height):
-        """ initialize function
-            Args:
-                width (int): width of rec
-                height (int): height of rec 
-        """
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
-
-    def __str__(self):
-        """ string formatter """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
