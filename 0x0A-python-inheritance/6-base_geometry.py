5fc72b34d04f65db0d2210bfcc0ebd2470c3af9a
Tshegofatso Morare <tshego.tt.morare@gmail.com>
2023-09-11 18:24:35 +0000

    inheritance

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -2,6 +2,8 @@
 
 """ improved base geomtry class with area function """
 class BaseGeometry:
+    """ class  base geometry """
     def area(self):
+        """ area """
         raise Exception("area() is not implemented")
 
