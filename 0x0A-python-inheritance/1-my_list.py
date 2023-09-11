6e926f64f47ed326bdead9c6f09c348e07ae65dd
Tshegofatso Morare <tshego.tt.morare@gmail.com>
2023-09-11 18:03:16 +0000

    inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,7 +5,12 @@
         list: the list that is going to be sorted
 """
 class MyList(list):
+    """ My list class
+    Args:
+        list: the list that is going to be sorted
+    """
     def print_sorted(self):
+        """ funct to print sorted list """
         sorted_list = sorted(self)
         print(sorted_list)
 
