3e083b7550f52eff006cc1c69419429124d4f484
Tshegofatso Morare <tshego.tt.morare@gmail.com>
2023-09-11 18:29:41 +0000

    inheritance

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -2,16 +2,17 @@
 
 """ base geometry class """
 class BaseGeometry:
-    """ area function """
+    """ base geometry class """
     def area(self):
+        """ area function """
         raise Exception("area() is not implemented")
 
-    """ integer_validator function
-        Args:
-            name (string): name string
-            value (int): int to be validated 
-    """
     def integer_validator(self, name, value):
+        """ integer_validator function
+            Args:
+                name (string): name string
+                value (int): int to be validated 
+        """
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
