64c9a368f845325a8bfe5c95019a5a343a90d186
Therushmorare <thesgo.tt.morare@gmail.com>
2023-11-19 15:41:28 -0800

    db fixes

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  lists all states from the database"""
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
@@ -14,3 +14,4 @@ if __name__ == "__main__":
         print(row)
     cur.close()
     db.close()
+
