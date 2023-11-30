6f47a81ce5f2c677c473d9540866747b0635d816
Therushmorare <thesgo.tt.morare@gmail.com>
2023-11-30 13:14:36 -0800

    task 1 and 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#task 0
 curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#task 1
 if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
