1ab573df45b2edb0dbac4e56ec278bd48b8f4cb0
Therushmorare <thesgo.tt.morare@gmail.com>
2023-11-30 13:04:03 -0800

    tasks

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
---
+++ 0x10-python-network_0/3-methods.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -s -I -X OPTIONS "$1" | grep 'Allow:' | cut -f2- -d' '
