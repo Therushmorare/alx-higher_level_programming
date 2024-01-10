c2a4e59432350066f689c5f2a589d93bfb3ad059
therushmorare <Tshego.tt.morare@gmail.com>
2024-01-09 16:53:12 -0800

    task 100

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-const episode = process.argv[2];
-const apiUrl = `http://swapi.co/api/films/${episode}`;
+const epId = process.argv[2];
+const apiUrl = `http://swapi.co/api/films/${epId}`;
 request.get(apiUrl, (error, response, body) => {
   if (error) console.log(error);
   console.log(JSON.parse(body).title);
